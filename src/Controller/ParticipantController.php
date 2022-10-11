7d30464efaaa366774cab1e6e748a288d6d34262
ydoucet2022 <doucet.yohann@gmail.com>
2022-10-11 10:09:57 +0200

    ajout du controller modifier profil + twig de l'espace user

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ParticipantRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    #[Route('/participant/modifier_profil/{id}',
+        name: 'app_participant',
+        requirements:['id'=>'\d+']
+    )]
+    public function modifierProfil(
+        int $id,
+        ParticipantRepository $participantRepository
+    ): Response
+    {
+        $user = $participantRepository->findOneBy(
+            ['id'=>$id]
+        );
+        return $this->render('participant/modifier_profil.html.twig',
+            [
+                'user' => $user,
+            ]);
+    }
+}
