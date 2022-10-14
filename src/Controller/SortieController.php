56b140d765e7d68eb05044939ba355a8f8ab4fb6
ydoucet2022 <doucet.yohann@gmail.com>
2022-10-14 11:00:44 +0200

    page annuler qui se met en place

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use App\Entity\Sortie;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
-use App\Repository\ParticipantRepository;
-use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,28 +12,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Services\SortieFiltres;
 
 #[Route('/sortie')]
 class SortieController extends AbstractController
 {
-    //METHODE D'AFFICHAGE DE LA PAGE
     #[Route('/', name: 'app_sortie_index', methods: ['GET'])]
     public function index(SortieRepository $sortieRepository,
                           EntityManagerInterface $entityManager,
-                          EtatRepository $etatRepository,
-                          SiteRepository $siteRepository
+                          EtatRepository $etatRepository
     ): Response
     {
-        //ARCHIVAGE DES SORTIES TERMINEES DEPUIS +1 MOIS
         $etatArchive = $etatRepository->findOneBy(
             [
                 'id'=>7
             ]
         );
+
         date_default_timezone_set('Europe/Paris');
         $dateNow = new \DateTime("now");
         $sorties = $sortieRepository->findAll();
+
         foreach ($sorties as $sort)
         {
             $finDeSortie = clone $sort->getDateHeureDebut();
@@ -49,50 +45,10 @@ class SortieController extends AbstractController
         }
 
         return $this->render('sortie/index.html.twig', [
-            'sorties' => $sorties,
-            'sites' => $siteRepository->findAll()
+            'sorties' => $sortieRepository->findAll(),
         ]);
     }
 
-    //METHODE POUR APPLICATION DES FILTRES
-    #[Route('/filtres', name: 'app_sortie_index_filtre', methods: ['POST'])]
-    public function indexFiltre(SortieRepository $sortieRepository,
-                                SortieFiltres $sortieFiltres,
-                                SiteRepository $siteRepository,
-                                ParticipantRepository $participantRepository,
-                                Request $request
-    ): Response
-    {
-        //GESTION DES FILTRES
-        $siteSelection = $request->request->get('site_filter');
-        $rechercheTexte = $request->request->get('word_filter');
-        $dateDebut = $request->request->get('dateDebut_filter');
-        $dateFin = $request->request->get('dateFin_filter');
-        $suisOrganisateur = $request->request->get('suisOrganisateur');
-        $suisInscrit = $request->request->get('suisInscrit');
-        $suisPasInscrit = $request->request->get('suisPasInscrit');
-        $sortiesPassees = $request->request->get('sortiesPassees');
-
-        $user = $participantRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]) ;
-        $userId = $user->getId();
-
-        $sorties = $sortieRepository->findWithFilters($siteSelection,
-                                    $rechercheTexte,
-                                    $dateDebut,
-                                    $dateFin,
-                                    $suisOrganisateur,
-                                    $suisInscrit,
-                                    $suisPasInscrit,
-                                    $sortiesPassees,
-                                    $userId);
-
-        return $this->render('sortie/index.html.twig', [
-            'sorties' => $sorties,
-            'sites' => $siteRepository->findAll()
-        ]);
-    }
-
-    //METHODE POUR PASSER UNE SORTIE EN PUBLIEE
     #[Route('/{id}', name: 'app_sortie_index_pub', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function indexPublier(SortieRepository $sortieRepository, EtatRepository $etatRepository, int $id): Response
     {
@@ -101,15 +57,27 @@ class SortieController extends AbstractController
         );
         $etat = $etatRepository->findOneBy([
             'id'=>2
+
         ]);
         $sortie->setEtatSortie($etat);
         $sortieRepository->save($sortie, true);
+
         return $this->render('sortie/index.html.twig', [
             'sorties' => $sortieRepository->findAll(),
         ]);
     }
 
-    //METHODE DE CREATION D'UNE NOUVELLE SORTIE
+    #[Route('/', name: 'app_sortie_index_filtre', methods: ['POST'])]
+    public function indexFiltre(SortieRepository $sortieRepository): Response
+    {
+
+        return $this->render('sortie/index.html.twig', [
+            'sorties' => $sortieRepository->findBy(
+                ['site' => $_POST['site_filter']]
+            ),
+        ]);
+    }
+
     #[Route('/new', name: 'app_sortie_new', methods: ['GET', 'POST'])]
     public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository): Response
     {
@@ -132,11 +100,10 @@ class SortieController extends AbstractController
 
         return $this->renderForm('sortie/new.html.twig', [
             'sortie' => $sortie,
-            'form' => $form
+            'form' => $form,
         ]);
     }
 
-    //METHODE POUR AFFICHER LES DETAILS D'UNE SORTIE
     #[Route('/details/{id}', name: 'app_sortie_show', methods: ['GET'])]
     public function show(Sortie $sortie): Response
     {
@@ -151,24 +118,42 @@ class SortieController extends AbstractController
     {
         $form = $this->createForm(SortieType::class, $sortie);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $sortieRepository->save($sortie, true);
 
             return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
         }
+
         return $this->renderForm('sortie/edit.html.twig', [
             'sortie' => $sortie,
-            'form' => $form
+            'form' => $form,
         ]);
     }
 
-    //METHODE POUR ANNULER UNE SORTIE EN TANT QU'ORGANISATEUR
-    #[Route('/annuler/{id}', name: 'app_sortie_cancel', methods: ['POST'])]
-    public function cancel(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    #[Route('/annuler/{id}', name: 'app_sortie_cancel', methods: ['GET','POST'])]
+    public function cancel(Request $request, Sortie $sortie, SortieRepository $sortieRepository,
+                           EtatRepository $etatRepository): Response
     {
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $etat = $etatRepository->findOneBy(
+                [
+                    'id'=> 6
+                ]
+            );
+            $sortie->setEtatSortie($etat);
 
-        return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+            $sortieRepository->save($sortie,true);
+            return $this->redirectToRoute('app_sortie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/cancel.html.twig', [
+            'sortie' => $sortie,
+            'form'=>$form,
+        ]);
     }
 
     #[Route('/{id}', name: 'app_sortie_delete', methods: ['POST'])]
