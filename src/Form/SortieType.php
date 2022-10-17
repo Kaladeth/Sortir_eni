7a52b33831cef1fed51a241ab4b42f73188e5e68
ejardin2022 <esteban.jardin2022@campus-eni.fr>
2022-10-17 17:27:44 +0200

    Last commit of monday

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,19 +2,17 @@
 
 namespace App\Form;
 
+use App\Entity\Categorie;
 use App\Entity\Lieu;
 use App\Entity\Site;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
-use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Validator\Constraints\GreaterThan;
 
 class SortieType extends AbstractType
 {
@@ -23,30 +21,34 @@ class SortieType extends AbstractType
         $builder
             ->add('nom', null,
                 [
-
                 ])
             ->add('dateHeureDebut', null,
                 [
                     "attr"=>["class"=>"dhd_ajout"],
-//                    'constraints'=>
-//                    [
-//                        new DateTime()
-//                    ]
-
+                    "label"=>"Date et heure de la sortie"
+                ])
+            ->add('duree', null,
+                [
+                    "attr"=>["placeholder"=>"en minutes"],
+                    "label"=>"Durée"
                 ])
             ->add('dateLimiteInscription', null,
                 [
-                    "attr"=>["class"=>"dli_ajout"]
+                    "attr"=>["class"=>"dli_ajout"],
+                    "label"=>"Date limite d'inscription"
                 ])
             ->add('nbInscriptionsMax',null,
                 [
+                    "label"=>"Nombre maximum d'inscrits"
                 ])
             ->add('infosSortie', TextareaType::class,
                 [
-                    "attr"=>["class"=>"motifAnnulation"]
+                    "attr"=>["class"=>"motifAnnulation"],
+                    "label"=>"Description de la sortie"
                 ])
             ->add('imageFile',VichImageType::class,
                 [
+                    "label"=>"Photo"
                 ])
             ->add('lieu', EntityType::class,
                 [
