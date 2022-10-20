a22b0f1642bfe9ee016789f051a46a49c67366b3
ejardin2022 <esteban.jardin2022@campus-eni.fr>
2022-10-20 15:38:46 +0200

    Harmonisation du front part.2

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -32,7 +32,7 @@ class VilleController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $villeRepository->save($ville, true);
-            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('accueil_main', [], Response::HTTP_SEE_OTHER);
         }
             return $this->renderForm('ville/new.html.twig', [
             'ville' => $ville,
